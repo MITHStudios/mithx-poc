b2579f2cc8f799e264adbea6b8e14c8f8b088622
mortelli <ortellimarcelo@gmail.com>
2023-06-14 19:54:33 -0300

    add auth0 sample project (2)

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <h1 className="mb-4">MITHx PoC</h1>
+
+    <p className="lead">
+      This is a sample application that validates basic functionalities and architecture for MITHx.
+    </p>
+  </div>
+);
+
+export default Hero;
