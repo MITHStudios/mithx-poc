a99de8bbc9251490c8297077f5de5f96494770ca
mortelli <ortellimarcelo@gmail.com>
2023-06-20 19:33:03 -0300

    fix typo in database setting

--- api-server.js
+++ api-server.js
@@ -19,7 +19,7 @@ const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABSE,
+  database: process.env.MYSQL_DATABASE,
 });
 connection.connect((err) => {
   if (err) {
